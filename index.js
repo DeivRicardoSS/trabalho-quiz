0fad705529d3f81259c165c2383f55245ee8d6ab
Deivyson Ricardo Silva dos Santos <131066052+DeivRicardoSS@users.noreply.github.com>
2025-04-04 10:29:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import opt from 'https://deivyson.me/optmization-js/src/index.js';
+import opt from 'http://deivyson.me/optmization-js/src/index.js';
 const { on, getDOMValue, setData, redirect } = opt;
 
 on('submit', 'form', (e)=>{
